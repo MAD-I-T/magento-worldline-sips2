9adbd6a6fe3edf4cde513bc7af99f2450721afdf
Mamadou <seyuf@hotmail.com>
2022-04-30 16:31:10 +0200

    Update unit-test.sh

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -19,7 +19,7 @@ ls -lath dev/tests/unit/
 ./vendor/phpunit/phpunit/phpunit "$PROJECT_PATH/magento/app/code/Madit/Atos"
 ./vendor/phpunit/phpunit/phpunit "$PROJECT_PATH/magento/app/code/Madit/Atos/Test/Unit/Block/DebugTest.php"
 
-
+ls -lath "$PROJECT_PATH/magento/app/code/Madit/Atos/Test/Unit/Block/"
 
 
 if [ -n "$INPUT_UNIT_TEST_SUBSET_PATH" ]
