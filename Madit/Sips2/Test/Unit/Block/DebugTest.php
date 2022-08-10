fd53e603ecb3aaa38a07106e61a4464c58dd6fb6
seyuf <seyuf@hotmail.com>
2022-08-10 14:55:46 +0200

    back to individual unit testing

---
+++ Madit/Sips2/Test/Unit/Block/DebugTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Madit\Sips2\Test\Unit\Block;
+use PHPUnit\Framework\TestCase;
+
+class DebugTest extends TestCase
+{
+  
+    public function testTrue(){
+        $this->assertTrue(true, true);
+    }
+
+}
